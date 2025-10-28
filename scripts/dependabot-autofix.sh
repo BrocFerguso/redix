1e439354733249262717daebeeea1fb759f8412f
dengpingya <127326485+dengpingya@users.noreply.github.com>
2025-10-28 10:42:21 +0800

    Update dependabot-autofix.sh

--- scripts/dependabot-autofix.sh
+++ scripts/dependabot-autofix.sh
@@ -8,3 +8,20 @@ if [[ `git status --porcelain --untracked-files=no` ]]; then
 else
 	echo "no changes detected"  
 fi
+  // We need to release the mutexes to be able to restart the application
+    if (msi_mutex)
+    {
+        msi_mutex.reset(nullptr);
+    }
+
+    if (is_restart_scheduled())
+    {
+        if (!restart_if_scheduled())
+        {
+            // If it's not possible to restart non-elevated due to some condition in the user's configuration, user should start PowerToys manually.
+            Logger::warn("Scheduled restart failed. Couldn't restart non-elevated. PowerToys exits here because retrying it would just mean failing in a loop.");
+        }
+    }
+    stop_tray_icon();
+    return result;
+}
